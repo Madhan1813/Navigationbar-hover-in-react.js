85aa1525b85e2e358f696dd2b1ffad34a0e663d9
MADHAN R <madhanravi63@gmail.com>
2023-10-12 10:16:00 +0530

    navigationbar bar commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../components/Nav.css'
+
+const Nav = () => {
+  return (
+    
+    <div class="bar">
+        <a href="#">Home</a>
+        <a href="#">About us</a>
+        <a href="#">jobs</a>
+        <a href="#">login</a>
+        <a href="#">service</a>
+        <span></span>
+    </div>
+  )
+}
+
+export default Nav
